ee04f2b4c26c693a4c44b6432e499000b2b6b96d
Eugene Yokota <eed3si9n@gmail.com>
2013-06-02 03:16:37 -0400

    wip

--- src/main/scala/scopt/generic/options.scala
+++ src/main/scala/scopt/generic/options.scala
@@ -4,7 +4,6 @@ import GenericOptionParser.UNBOUNDED
 
 trait OptionDefKind {}
 case object Opt extends OptionDefKind
-case object KVOpt extends OptionDefKind
 case object Arg extends OptionDefKind
 case object Sep extends OptionDefKind
 
@@ -47,19 +46,20 @@ private[scopt] abstract class OptionDefinition[A: Read, C] {
   def name: Option[String]
   def nameOrBlank: String = name.getOrElse("")
   def _shortOpt: Option[Char]
+  def shortOptOrBlank: String = _shortOpt map {_.toString} getOrElse("")
   def maxOccurs: Int
   def minOccurs: Int
   def canBeInvoked: Boolean =
     kind match {
-      case Opt|KVOpt => true
-      case _         => false
+      case Opt => true
+      case _   => false
     }
   def shortDescription: String =
     kind match {
-      case Opt|KVOpt => "option " + nameOrBlank
-      case _         => "argument " + nameOrBlank
+      case Opt => "option " + nameOrBlank
+      case _   => "argument " + nameOrBlank
     }
-  def keyValueArgument: Boolean = kind == KVOpt
+  // def keyValueArgument: Boolean = kind == KVOpt
   def gobbleNextArgument: Boolean = kind == Opt
   def callback: (A, C) => C
   def applyArgument(arg: String, config: C): Option[C]  =
@@ -73,6 +73,38 @@ private[scopt] abstract class OptionDefinition[A: Read, C] {
         System.err.println("Error: " + shortDescription + " failed when given '" + arg + "'. " + e.getMessage)
         None
     }
+  // number of tokens to read: 0 or no match, 2 for "--foo 1", 1 for "--foo:1"
+  def shortOptTokens(arg: String): Int =
+    _shortOpt match {
+      case Some(c) if arg == "-" + shortOptOrBlank                 => 2
+      case Some(c) if arg startsWith ("-" + shortOptOrBlank + ":") => 1
+      case _ => 0
+    }
+  def longOptTokens(arg: String): Int =
+    if (arg == "--" + nameOrBlank) 2
+    else if (arg startsWith ("--" + nameOrBlank + ":")) 1
+    else 0
+  def tokensToRead(i: Int, args: Seq[String]): Int =
+    if (i >= args.length || kind != Opt) 0
+    else args(i) match {
+      case arg if longOptTokens(arg) > 0  => longOptTokens(arg)
+      case arg if shortOptTokens(arg) > 0 => shortOptTokens(arg) 
+      case _ => 0
+    }
+  def apply(i: Int, args: Seq[String]): Either[String, String] =
+    if (i >= args.length || kind != Opt) Left("Option does not match")
+    else args(i) match {
+      case arg if longOptTokens(arg) == 2 || shortOptTokens(arg) == 2 =>
+        token(i + 1, args) map {Right(_)} getOrElse Left("Missing value after '" + arg + "'")
+      case arg if longOptTokens(arg) == 1 =>
+        Right(arg drop ("--" + nameOrBlank + ":").length)
+      case arg if shortOptTokens(arg) == 1 =>
+        Right(arg drop ("-" + shortOptOrBlank + ":").length)
+      case _ => Right("")
+    }
+  def token(i: Int, args: Seq[String]): Option[String] =
+    if (i >= args.length || kind != Opt) None
+    else Some(args(i))
 }
 
 // // ----- Some standard option types ---------
@@ -92,119 +124,6 @@ private[scopt] abstract class OptionDefinition[A: Read, C] {
 
 //   override def shortDescription = "argument " + name
 // }
-
-// private[scopt] class ArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         valueName: String,
-//         description: String,
-//         action: (String, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, null, valueName,
-//           description, action, true, false, 0, UNBOUNDED)
-
-// private[scopt] class IntArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         valueName: String,
-//         description: String,
-//         action: (Int, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, null, valueName,
-//           description, { (a: String, c: C) => action(a.toInt, c) }, true, false, 0, UNBOUNDED)
-
-// private[scopt] class DoubleArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         valueName: String,
-//         description: String,
-//         action: (Double, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, null, valueName,
-//           description, { (a: String, c: C) => action(a.toDouble, c) }, true, false, 0, UNBOUNDED)
-
-// private[scopt] class BooleanArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         valueName: String,
-//         description: String,
-//         action: (Boolean, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, null, valueName, 
-//           description, { (a: String, c: C) =>
-//     val boolValue = a.toLowerCase match {
-//       case "true" => true
-//       case "false" => false
-//       case "yes" => true
-//       case "no" => false
-//       case "1" => true
-//       case "0" => false
-//       case _ =>
-//         throw new IllegalArgumentException("Expected a string I can interpret as a boolean")
-//     }
-//     action(boolValue, c) },
-//     true, false, 0, UNBOUNDED)
-    
-// private[scopt] object KeyValueParser {
-//   def split(s: String): (String, String) = s.indexOf('=') match {
-//     case -1     => throw new IllegalArgumentException("Expected a key=value pair")
-//     case n: Int => (s.slice(0, n), s.slice(n + 1, s.length))
-//   }
-// }
-
-// private[scopt] class KeyValueArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         keyName: String,
-//         valueName: String,
-//         description: String,
-//         action: (String, String, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, keyName, valueName, 
-//           description, { (a: String, c: C) => action(KeyValueParser.split(a)._1, KeyValueParser.split(a)._2, c) },
-//           false, true, 0, UNBOUNDED)
-
-// private[scopt] class KeyIntValueArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         keyName: String,
-//         valueName: String,
-//         description: String,
-//         action: (String, Int, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, keyName, valueName, 
-//           description, { (a: String, c: C) => action(KeyValueParser.split(a)._1, KeyValueParser.split(a)._2.toInt, c) },
-//           false, true, 0, UNBOUNDED)
-
-// private[scopt] class KeyDoubleValueArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         keyName: String,
-//         valueName: String,
-//         description: String,
-//         action: (String, Double, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, keyName, valueName, 
-//           description, { (a: String, c: C) => action(KeyValueParser.split(a)._1, KeyValueParser.split(a)._2.toDouble, c) },
-//           false, true, 0, UNBOUNDED)
-
-// private[scopt] class KeyBooleanValueArgOptionDefinition[C](
-//         shortopt: Option[String],
-//         longopt: String,
-//         keyName: String,
-//         valueName: String,
-//         description: String,
-//         action: (String, Boolean, C) => C
-//         ) extends OptionDefinition[C](true, shortopt, longopt, null, valueName, 
-//           description, { (a: String, c: C) => 
-//             val x = KeyValueParser.split(a)
-//             val key = x._1
-//             val boolValue = x._2.toLowerCase match {
-//               case "true" => true
-//               case "false" => false
-//               case "yes" => true
-//               case "no" => false
-//               case "1" => true
-//               case "0" => false
-//               case _ =>
-//                 throw new IllegalArgumentException("Expected a string I can interpret as a boolean")
-//             }
-//             action(key, boolValue, c)
-//           },
-//           false, true, 0, UNBOUNDED)
       
 // private[scopt] class FlagOptionDefinition[C](
 //         shortopt: Option[String],
@@ -271,24 +190,14 @@ private[scopt] trait GenericOptionParser[C] {
     var i = 0
     val unseenArgs = ListBuffer() ++ arguments
     var argListCount = 0
-    var indexOutOfBounds = false
     var _config: C = init
     var _error = false
 
     while (i < args.length) {
       val arg = args(i)
-      val matchingOption = opts.find(opt =>
-        opt.canBeInvoked &&
-          ((!opt.keyValueArgument &&
-            (arg == "--" + opt.name.getOrElse("") ||
-            (opt._shortOpt map { o => arg == "-" + o } getOrElse { false }))) ||
-          (opt.keyValueArgument &&
-            (arg.startsWith("--" + opt.name.getOrElse("") + ":") ||
-            (opt._shortOpt map { o => arg.startsWith("-" + o + ":") } getOrElse { false }))))
-      )
-      
-      matchingOption match {
-      //   case None =>
+
+      opts.find(_.tokensToRead(i, args) > 0) match {
+        case None =>
       //     if (arg.startsWith("-")) {
       //       if (errorOnUnknownArgument) {
       //         System.err.println("Error: Unknown argument '" + arg + "'")
@@ -316,34 +225,23 @@ private[scopt] trait GenericOptionParser[C] {
       //     }
           
         case Some(option) =>
-          val argToPass: String = if (option.gobbleNextArgument) {
-            i += 1;
-            
-            if (i >= args.length) {
-              indexOutOfBounds = true
+          option(i, args) match {
+            case Right(value) =>
+              option.applyArgument(value, _config) match {
+                case Some(c) => _config = c
+                case None    => _error = true
+              }
+            case Left(outOfBounds) =>
               if (errorOnUnknownArgument) {
-                System.err.println("Error: missing value after '" + arg + "'")
+                System.err.println("Error: " + outOfBounds)
                 _error = true
-              } else
-                System.err.println("Warning: missing value after '" + arg + "'")
-              ""
-            } else
-              args(i)
-          } else if (option.keyValueArgument &&
-              (option._shortOpt map { o => arg.startsWith("-" + o + ":") } getOrElse { false })) {
-            arg.drop(("-" + option._shortOpt.get + ":").length)
-          } else if (option.keyValueArgument &&
-              arg.startsWith("--" + option.name.getOrElse("") + ":")) {
-            arg.drop(("--" + option.name.getOrElse("") + ":").length)
-          } else
-            ""
-          
-          if (!indexOutOfBounds) {
-            option.applyArgument(argToPass, _config) match {
-              case Some(c) => _config = c
-              case None    => _error = true
-            }
+              }
+              else System.err.println("Warning: " + outOfBounds)
           }
+          // move index forward for gobbling
+          if (option.tokensToRead(i, args) > 1) {
+            i += option.tokensToRead(i, args) - 1
+          } // if
       }
       i += 1
     }
--- src/test/scala/scopt/MutableParserSpec.scala
+++ src/test/scala/scopt/MutableParserSpec.scala
@@ -4,67 +4,74 @@ class MutableParserSpec extends Specification { def is =      s2"""
   This is a specification to check the mutable parser
 
   opt[Int]("foo") action { x => x } should
-    parse 1 out of --foo 1                                      $e1
-  
+    parse 1 out of --foo 1                                      ${intParser("--foo", "1")}
+    parse 1 out of --foo:1                                      ${intParser("--foo:1")}
+    
   opt[String]("foo") action { x => x } should
-    parse "bar" out of --foo bar                                $e2
+    parse "bar" out of --foo bar                                ${stringParser("--foo", "bar")}
+    parse "bar" out of --foo:bar                                ${stringParser("--foo:bar")}
 
   opt[Double]("foo") action { x => x } should
-    parse 1.0 out of --foo 1.0                                  $e3
+    parse 1.0 out of --foo 1.0                                  ${doubleParser("--foo", "1.0")}
+    parse 1.0 out of --foo:1.0                                  ${doubleParser("--foo:1.0")}
 
   opt[Boolean]("foo") action { x => x } should
-    parse true out of --foo 1                                   $e4
+    parse true out of --foo true                                ${trueParser("--foo", "true")}
+    parse true out of --foo:true                                ${trueParser("--foo:true")}
+    parse true out of --foo 1                                   ${trueParser("--foo", "1")}
+    parse true out of --foo:1                                   ${trueParser("--foo:1")}
 
   opt[(String, Int)]("foo") action { x => x } should
-    parse ("k", 1) out of --foo k=1                             $e5    
+    parse ("k", 1) out of --foo k=1                             ${pairParser("--foo", "k=1")}
+    parse ("k", 1) out of --foo:k=1                             ${pairParser("--foo:k=1")}    
                                                                 """
 
-  def e1 = {
+  def intParser(args: String*) = {
     var foo = 0
-    val parser1 = new scopt.OptionParser("scopt") {
+    val parser = new scopt.OptionParser("scopt") {
       opt[Int]("foo") action { x => foo = x }
     }
-    parser1.parse(Seq("--foo", "1"))
+    parser.parse(args.toSeq)
     foo === 1
   }
 
-  def e2 = {
+  def stringParser(args: String*) = {
     var foo = ""
-    val parser1 = new scopt.OptionParser("scopt") {
+    val parser = new scopt.OptionParser("scopt") {
       opt[String]("foo") action { x => foo = x }
     }
-    parser1.parse(Seq("--foo", "bar"))
+    parser.parse(args.toSeq)
     foo === "bar"
   }
 
-  def e3 = {
+  def doubleParser(args: String*) = {
     var foo = 0.0
-    val parser1 = new scopt.OptionParser("scopt") {
+    val parser = new scopt.OptionParser("scopt") {
       opt[Double]("foo") action { x => foo = x }
     }
-    parser1.parse(Seq("--foo", "1.0"))
+    parser.parse(args.toSeq)
     foo === 1.0
   }
 
-  def e4 = {
-    var foo = 0.0
-    val parser1 = new scopt.OptionParser("scopt") {
-      opt[Double]("foo") action { x => foo = x }
+  def trueParser(args: String*) = {
+    var foo = false
+    val parser = new scopt.OptionParser("scopt") {
+      opt[Boolean]("foo") action { x => foo = x }
     }
-    parser1.parse(Seq("--foo", "1"))
-    foo === 1.0
+    parser.parse(args.toSeq)
+    foo === true
   }
 
-  def e5 = {
+  def pairParser(args: String*) = {
     var foo = ""
     var value = 0
-    val parser1 = new scopt.OptionParser("scopt") {
+    val parser = new scopt.OptionParser("scopt") {
       opt[(String, Int)]("foo") action { case (k, v) =>
         foo = k
         value = v
       }
     }
-    parser1.parse(Seq("--foo", "k=1"))
+    parser.parse(args.toSeq)
     (foo === "k") and (value === 1)
   }
 }
