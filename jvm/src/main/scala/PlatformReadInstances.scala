c28e16622dbe7d3c574415751280d83c0de9e86f
kururuken <shifp9707@gmail.com>
2022-07-02 12:27:18 -0700

    fix #318 locale is not working for calendar Read (#319)
    Fixes issue 318
    
    Co-authored-by: Fangping Shi <fangping_shi@apple.com>

--- jvm/src/main/scala/PlatformReadInstances.scala
+++ jvm/src/main/scala/PlatformReadInstances.scala
@@ -20,8 +20,8 @@ private[scopt] object platform {
     def calendarRead(pattern: String): Read[Calendar] = calendarRead(pattern, Locale.getDefault)
     def calendarRead(pattern: String, locale: Locale): Read[Calendar] =
       Read.reads { s =>
-        val fmt = new SimpleDateFormat(pattern)
-        val c = new GregorianCalendar
+        val fmt = new SimpleDateFormat(pattern, locale)
+        val c = new GregorianCalendar(locale)
         c.setTime(fmt.parse(s))
         c
       }
